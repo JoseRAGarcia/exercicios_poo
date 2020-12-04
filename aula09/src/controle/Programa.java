14c7f50b3634291bce15cffd644c52460293f344
Jose Garcia <jose.ragarcia82@gmail.com>
2020-12-03 21:22:24 -0300

    Exercícios da aula de POO na UNIGRANRIO

---
+++ aula09/src/controle/Programa.java
@@ -0,0 +1,11 @@
+package controle;
+
+import face.JanelaTime;
+
+public class Programa {
+
+    public static void main(String[] args) {
+        JanelaTime jt = new JanelaTime();
+        jt.setVisible(true);
+    }
+}
---
+++ aula11/src/controle/Programa4.java
@@ -0,0 +1,22 @@
+package controle;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class Programa4 {
+    public static void main(String[] args) {
+        TreeSet<String> nomes = new TreeSet<String>();
+
+        nomes.add("José");
+        nomes.add("Maria");
+        nomes.add("Dalira");
+        nomes.add("Ralf");
+        nomes.add("Gerson");
+
+        System.out.println("LISTA 1 DE NOMES: ");
+        Iterator it = nomes.iterator();
+        while (it.hasNext())
+            System.out.print(it.next() + " - ");
+
+    }
+}
